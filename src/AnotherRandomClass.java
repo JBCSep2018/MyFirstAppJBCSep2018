df20dc218372aa5b441038629bbaf3f70c7c10a7
Someone else <afua.ankomah@gmail.com>
2018-09-18 17:02:51 -0400

    Initial commit

---
+++ src/AnotherRandomClass.java
@@ -0,0 +1,57 @@
+public class AnotherRandomClass {
+    public static void main(String[] args) {
+        // This is my first Java Application with Fi
+        //        This is my second comment in that application
+        System.out.println("Hello From Another Random Class!");
+        /*Multi line comment
+        * I can put my comments on many lines
+        * As many as I want */
+
+
+        for(int count=1;count<11; count++)
+        {
+
+            if(count==5)
+            {
+                System.out.println("Wooohoooooo!!!");
+
+            }
+            else{
+                System.out.println(count);
+            }
+
+        }
+
+        int count = 1;
+        do{
+            if(count==5){
+                System.out.println("Woooooohooooo!!!");
+            }
+            else{
+                System.out.println(count);
+            }
+            count++;
+
+        }while(count<=10);
+
+
+        count =1;
+        while(count<=10)
+        {
+            if(count==5){
+                System.out.println("Wooohooo!");
+            }else{
+
+                System.out.println(count);
+            }
+            count++;
+
+        }
+
+
+
+
+    }
+
+
+}
